1926420b2934d2b980746fdd641f7bf16f8b14da
Yannick VANAT <yvanat@e2r8p7.42.fr>
2020-02-12 05:04:24 +0100

    hop

--- py/p22.py
+++ py/p22.py
@@ -1,25 +1,17 @@
-abundant, sum_abundant = [], []
-sum1 = 276
+abundant, sum_abundant = [], [0] * 56250
+sum1 = 0
 for i in range(2, 28124):
-	print("{}".format(i))
 	sum1 = 1
 	for j in range(2, i):
-		if (i % j == 0):
+		if i % j == 0:
 			sum1 += j
 	if i < sum1:
 		abundant.append(i)
-sum1 = 0
 for a1 in abundant:
 	for a2 in abundant:
-		sum_abundant.append(a1 + a2)
-sorted(sum_abundant)
-for i in range(0, len(sum_abundant) - 1):
-	if sum_abundant[i] == sum_abundant[i + 1]:
-		del sum_abundant[i]
-for i in range(0, len(sum_abundant) - 1):
-	if sum_abundant[i + 1] - sum_abundant[i] != 1:
-		insert(i + 1, 0)
-for i in range(24, 28124):
-	if not bool(abundant[i - 24]):
-		sum += i
+		sum_abundant[a1 + a2] = 1
+sum1 = 0
+for i in range(1, 28124):
+	if sum_abundant[i] == 0:
+		sum1 += i
 print("{}".format(sum1))
